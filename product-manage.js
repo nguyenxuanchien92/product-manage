a92150028a8e719d608ed973ea73ec4ee43c547d
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-21 10:17:34 +0700

    vẫn chưa hiểu rõ  OOP...

--- product-manage.js
+++ product-manage.js
@@ -1,20 +1,21 @@
 class Product {
-    constructor(id, name, price) {
+    constructor(id, name, price, desc) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.desc = desc;
     }
 
     getHTML() {
         let str = "<th>" + "Name" + "</th>" + "<th>" + "Price" + "</th>" + "<th>" + "Desc"
-            + "</th>"+ "<th colspan='2'>" + "Action" + "</th>";
+            + "</th>" + "<th colspan='2'>" + "Action" + "</th>";
 
         str = str + "<tr>" + "<td>" + "Apple" +
             "</td>" + "<td>" + "1000" +
             "</td>" + "<td>" + "News" +
-            "</td>" +"<td>" + "<button type='button'>"+ "Edit"+"</button>" +
-            "</td>" + "<td>" + "<button type='button'>"+ "Delete"+"</button>" +
-            "</td>" +"</tr>"
+            "</td>" + "<td>" + "<button type='button'>" + "Edit" + "</button>" +
+            "</td>" + "<td>" + "<button type='button'>" + "Delete" + "</button>" +
+            "</td>" + "</tr>"
 
         return str;
     }
@@ -22,6 +23,9 @@ class Product {
 
 
 let name = document.getElementById("name-product");
+let price = document.getElementById("price-product");
+let desc = document.getElementById("desc-product");
+let products = [];
 
 function displayListProduct() {
     let productList = new Product();
@@ -30,7 +34,8 @@ function displayListProduct() {
 }
 
 function addProduct() {
-
+    let id = Math.floor(Math.random()*100)+1;
+    let product = new Product(id,name.value,price.value,desc.value);
 }
 
 displayListProduct();
